ce0a84e7c993369dd32808e01fc5940d82403ef3
Maria Le <hp.mars.le@gmail.com>
2020-06-08 15:04:08 -0500

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,3 +1,53 @@
+import { combineReducers } from 'redux';
+import uuid from 'uuid';
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+
+export default rootReducer;
+
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book];
+ 
+    case "REMOVE_BOOK":
+      idx = state.findIndex(book => book.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+ 
+    default:
+      return state;
+  }
+}
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+ 
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(author => author.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, {authorName: action.book.authorName, id: uuid() }];
+      }
+ 
+    default:
+      return state;
+  }
+}
+/*
 export default function bookApp(
   state = {
     authors: [],
@@ -10,6 +60,7 @@ export default function bookApp(
     case "ADD_BOOK":
       return {
         ...state,
+        authors: [...state.authors],
         books: [...state.books, action.book]
       };
 
@@ -17,12 +68,14 @@ export default function bookApp(
       idx = state.books.findIndex(book => book.id === action.id);
       return {
         ...state,
+        authors: [...state.authors],
         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
       };
 
     case "ADD_AUTHOR":
       return {
         ...state,
+        books: [...state.books],
         authors: [...state.authors, action.author]
       };
 
@@ -30,6 +83,7 @@ export default function bookApp(
       idx = state.authors.findIndex(author => author.id === action.id);
       return {
         ...state,
+        books: [...state.books],
         authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
       };
 
@@ -37,3 +91,4 @@ export default function bookApp(
       return state;
   }
 }
+*/
